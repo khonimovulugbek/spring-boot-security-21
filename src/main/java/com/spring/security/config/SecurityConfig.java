d15c1f4385e095ea0eabc248ef54039c96cb771e
Dark <gitlab_admin_da64f6@example.com>
2024-10-12 13:07:05 +0500

    feature : security filter chain configuration without security

---
+++ src/main/java/com/spring/security/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.spring.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    /**
+     * At this time security switch off
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.build();
+    }
+}
