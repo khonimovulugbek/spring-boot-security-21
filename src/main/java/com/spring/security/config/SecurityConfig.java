b5680e69719e2b59d37c4e0468abfdaf3412d25d
Dark <gitlab_admin_da64f6@example.com>
2024-10-12 13:22:45 +0500

    feature : Permitted any request by form login using default username and password (which is written in application.yml)

--- src/main/java/com/spring/security/config/SecurityConfig.java
+++ src/main/java/com/spring/security/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.spring.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,12 +12,13 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     /**
-     * Disable csrf and authenticate any request
+     * Permitted any request by form login using default username and password (which is written in application.yml).
      */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(customizer -> customizer.disable());
         http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+        http.formLogin(Customizer.withDefaults());
 
         return http.build();
     }
