259c4a95c0803ad85dc4e1bb7406828cdd4235fa
Dark <gitlab_admin_da64f6@example.com>
2024-10-12 23:09:42 +0500

    feature : auth with created UserDetails and set it to InMemoryUserDetailsManager

--- src/main/java/com/spring/security/config/SecurityConfig.java
+++ src/main/java/com/spring/security/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -28,6 +30,19 @@ public class SecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService(){
-        return new InMemoryUserDetailsManager(); // TODO: unauthorized
+        UserDetails user1 = User
+                .withDefaultPasswordEncoder()
+                .username("ali")
+                .password("123")
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User
+                .withDefaultPasswordEncoder()
+                .username("usmon")
+                .password("1234")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user1, user2); // TODO: auth with user1 and user2
     }
 }
